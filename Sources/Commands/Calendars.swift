50cbf33f0e90102566c78d17adf172ef900955f2
leoshimo <56844000+leoshimo@users.noreply.github.com>
2024-04-18 22:04:19 -0700

    feat: Add id-only output

--- Sources/Commands/Calendars.swift
+++ Sources/Commands/Calendars.swift
@@ -12,43 +12,62 @@ struct Calendars: AsyncParsableCommand {
     static var configuration = CommandConfiguration(commandName: "cal",
                                                     abstract: "Calendar commands",
                                                     subcommands: [
-                                                        GetCalendars.self,
+                                                        Get.self,
                                                     ],
-                                                    defaultSubcommand: GetCalendars.self)
+                                                    defaultSubcommand: Get.self)
+    
+    enum OutputFormat: String, ExpressibleByArgument, CaseIterable {
+        case human, id
+    }
+    
+    struct Get: AsyncParsableCommand {
+        static var configuration = CommandConfiguration(commandName: "get",
+                                                        abstract: "Get calendars")
+        
+        @Flag(help: "Return the default calendar")
+        var `default`: Bool = false
+        
+        @Option(name: .shortAndLong, help: "Return calendar with matching title")
+        var title: String?
+        
+        @Option(name: .shortAndLong, help: "Output format")
+        var format: OutputFormat = .human
+        
+        func run() async throws {
+            let store = EKEventStore()
+            
+            let calendars: [EKCalendar] = if `default` {
+                if let defaultCalendar = store.defaultCalendarForNewEvents {
+                    [defaultCalendar]
+                } else {
+                    []
+                }
+            } else if let title {
+                store.calendars(for: .event).filter { cal in cal.title == title }
+            } else {
+                store.calendars(for: .event)
+            }
+            
+            if calendars.isEmpty {
+                throw CLI.Err.NoCalendarsFound
+            }
+            
+            for c in calendars {
+                print(c.asString(in: format))
+            }
+        }
+        
+    }
+    
 }
 
-struct GetCalendars: AsyncParsableCommand {
-    static var configuration = CommandConfiguration(commandName: "get",
-                                                    abstract: "Get calendars")
-    
-    @Flag(help: "Return the default calendar")
-    var `default`: Bool = false
-    
-    @Option(name: .shortAndLong, help: "Return calendar with matching title")
-    var title: String?
-    
-    func run() async throws {
-        let store = EKEventStore()
-        
-        let calendars: [EKCalendar] = if `default` {
-            if let defaultCalendar = store.defaultCalendarForNewEvents {
-                [defaultCalendar]
-            } else {
-                []
-            }
-        } else if let title {
-            store.calendars(for: .event).filter { cal in cal.title == title }
-        } else {
-            store.calendars(for: .event)
-        }
-        
-        if calendars.isEmpty {
-            throw CLI.Err.NoCalendarsFound
-        }
-        
-        for c in calendars {
-            print("\(c.calendarIdentifier) - \(c.title)")
+extension EKCalendar {
+    func asString(in format: Calendars.OutputFormat) -> String {
+        switch format {
+        case .human:
+            "\(calendarIdentifier) - \(title)"
+        case .id:
+            "\(calendarIdentifier)"
         }
     }
 }
-
